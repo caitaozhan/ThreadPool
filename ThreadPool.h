5e50783400c2cb24c683c8c7e5903d7f0971e891
caitaozhan <caitaozhan@163.com>
2016-07-26 20:37:22 +0800

    first commit

---
+++ ThreadPool.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class ThreadPool
+{
+public:
+	ThreadPool(int n) {};
+};
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <future>
+#include <thread>
+#include <chrono>
+
+#include "ThreadPool.h"
+
+int main()
+{
+	ThreadPool pool(4);
+	return 0;
+}
